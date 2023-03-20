1605683f07f83d4df7ce467da68108f604228f26
HuyC <huy.com.hg@gmail.com>
2023-03-20 17:47:19 +0700

    Update main menu and pause menu

--- FinalProject/Assets/Scripts/PauseMenu.cs
+++ FinalProject/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -8,7 +9,13 @@ public class PauseMenu : MonoBehaviour
     [SerializeField]
     private GameObject pauseMenu;
     private bool isPause;
+    private GameObject pauseButton;
     // Update is called once per frame
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton = GameObject.FindGameObjectWithTag("PauseButton");
+    }
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -21,17 +28,24 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
-    private void pauseGame()
+    public void pauseGame()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPause = true;
+        pauseButton.SetActive(false);
     }
-    private void resumeGame()
+    public void resumeGame()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPause = false;
+        pauseButton.SetActive(true);
+    }
+    public void goToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
     }
     
 }
